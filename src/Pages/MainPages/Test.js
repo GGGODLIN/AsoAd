0f99225d4be8aead14fe53a65462d21f75d1c3dc
GGGODLIN <qwe70301@gmail.com>
2020-08-03 10:21:12 +0800

    0803

--- src/Pages/MainPages/Test.js
+++ src/Pages/MainPages/Test.js
@@ -32,6 +32,25 @@ import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
+const overTheme = createMuiTheme({
+    overrides: {
+        // Style sheet name ⚛️
+        MuiRadio: {
+            // Name of the rule
+            root: {
+                // Some CSS
+                background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+                borderRadius: 3,
+                border: 0,
+                color: 'white',
+                height: 48,
+                padding: '0 30px',
+                boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+            },
+        },
+    },
+});
+
 export const Test = (props) => {
 
     const { APIUrl, Theme } = useContext(Context);
